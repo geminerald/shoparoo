e0dd35286fbb0a48e1cee7799f20d34ec9b0eb99
geminerald <zacdwarner@gmail.com>
2020-05-20 14:44:47 +0000

    added cart html page

--- cart/contexts.py
+++ cart/contexts.py
@@ -13,7 +13,7 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
-    for id, quantity in cart_items():
+    for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
         product_count += quantity
