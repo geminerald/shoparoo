77c00588c1a9092528571eb23766b639d3f518e9
geminerald <zacdwarner@gmail.com>
2020-05-21 14:10:39 +0000

    added search functionality

--- search/views.py
+++ search/views.py
@@ -3,6 +3,9 @@ from products.models import Product
 
 # Create your views here.
 
+
 def do_search(request):
-    products = Products.objects.filter(name__icontains=request.GET['q'])
+    """ function to search for a product
+    """
+    products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, 'products.html', {'products':products})
